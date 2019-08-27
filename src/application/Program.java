dade5510127b7c049eabe27d5b0d350f9f1d9750
upeloudi <bernardoluizvalle@gmail.com>
2019-08-27 20:47:57 -0300

    Aula 11 tipos primitivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Hello Word!");
+
 		
-//		if (){
-//			
-//		}
-		// TODO Auto-generated method stub
+		boolean verdade = false;
+		char genero ='M';
+		byte x = 126;
+		
 
 	}
 
